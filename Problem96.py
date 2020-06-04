7d335b5cb695e7d4a967ffbe1bdb122ffa3fd4db
BruceMD <maxbruce9@gmail.com>
2020-06-04 13:19:24 +0900

    Extract info from .txt file, convert(name) for string to integers and gridGen puts all the numbers into a list in list format [[]]

--- Problem96.py
+++ Problem96.py
@@ -11,6 +11,28 @@ def main():
 				sudokuDic[tempKey] = []
 			else:
 				sudokuDic[tempKey] += convert(line.strip("\n"))
+	
+	print(gridGen(sudokuDic["Grid 01"]))
+
+def gridGen(numLis):
+	grid = []
+	tempLine = []
+	for i in range(81):
+		if numLis[i] == 0:
+			tempLine.append([1, 2, 3, 4, 5, 6, 7, 8, 9])
+		else:
+			tempLine.append([numLis[i]])
+		if (i + 1) % 9 == 0:
+			grid.append(tempLine)
+			tempLine = []
+	#			print(grid)
+	return grid
+
+def convert(name):
+	lis = []
+	for c in name:
+		lis.append(int(c))
+	return lis
 
 
 if __name__ == '__main__':
