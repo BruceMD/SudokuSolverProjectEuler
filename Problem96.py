ea16527f914ccddf827a11902309bda609c73f53
BruceMD <maxbruce9@gmail.com>
2020-06-04 14:19:43 +0900

    Created a recursive solve method to wittle down the options of each cell, repeating after every single change that it has made. Lost efficiency but pretty accurate. Along with solve(), there is resolve() to return true if there are clashes in the rows, cols or blocks

--- Problem96.py
+++ Problem96.py
@@ -12,7 +12,41 @@ def main():
 			else:
 				sudokuDic[tempKey] += convert(line.strip("\n"))
 	
+	
 	printGrid(gridGen(sudokuDic["Grid 01"]))
+	print()
+	printGrid(solve(gridGen(sudokuDic["Grid 01"])))
+	
+def solve(grid):					# get all options for all numbers in each cell
+#	printGridFull(grid)
+#	print()
+	for i in range(9):
+		for j in range(9):
+			if len(grid[i][j]) > 1:
+				for num in grid[i][j]:
+					if resolve(num, i, j, grid):
+						grid[i][j].remove(num)
+						return solve(grid)
+	
+	return grid
+	
+def resolve(num, i, j, grid):			# if num at i, j matches anything in the rows, columns or blocks then return True, there is a match, remove it from list of options
+	for k in range(9):
+		if [num] == grid[k][j] or [num] == grid[i][k]:
+			return True
+	for l in block(i):
+		for m in block(j):
+			if [num] == grid[l][m]:
+				return True
+	return False
+
+def block(num):
+	if num in [0, 1, 2]:
+		return [0, 1, 2]
+	elif num in [3, 4, 5]:
+		return [3, 4, 5]
+	else:
+		return [6, 7, 8]
 
 def printGrid(grid):
 	for line in grid:
@@ -22,6 +56,15 @@ def printGrid(grid):
 			else:
 				print(num[0], end="|")
 		print("")
+
+def printGridFull(grid):
+	for line in grid:
+		for num in line:
+			if len(num) > 1:
+				print(num, end="|")
+			else:
+				print(num[0], end="|")
+		print()
 
 def gridGen(numLis):
 	grid = []
